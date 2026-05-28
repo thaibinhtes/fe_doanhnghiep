5d032189c7d6e060cfee65d7dbde163b2f6f7739
thaibinhpressify <thaibinhpressify@gmail.com>
2026-05-28 09:54:29 +0700

    update filter

--- src/views/Companies/IdentityCompanies.vue
+++ src/views/Companies/IdentityCompanies.vue
@@ -12,7 +12,7 @@
               v-model="search"
               type="text"
               placeholder="Tìm theo tên công ty hoặc MST..."
-              class="dark:bg-dark-900 h-10 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30"
+              class="dark:bg-dark-900 h-10 w-[200px] rounded-lg border border-gray-300 bg-transparent px-4 py-2 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30"
             />
             <button
               type="button"
